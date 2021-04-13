541cf2a2c554c0591ec17e08846cb93a4c4df0f2
Alexey Kolesnyk <monocledlama@gmail.com>
2021-04-14 01:43:22 +0300

    db created

--- EF6_CodeFirst/Program.cs
+++ EF6_CodeFirst/Program.cs
@@ -10,7 +10,22 @@ namespace EF6_CodeFirst
     {
         static void Main(string[] args)
         {
+            using(UserContest db = new UserContest())
+            {
+                User user1 = new User { Name = "Tom", Age = 32 };
+                User user2 = new User { Name = "Sam", Age = 26 };
 
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Objects are saved");
+
+                var users = db.Users;
+                Console.WriteLine("Users list:");
+                foreach (var user in users)
+                    Console.WriteLine("{0}.{1} - {2}", user.Id, user.Name, user.Age);
+                Console.ReadLine();
+            }
         }
     }
 }
