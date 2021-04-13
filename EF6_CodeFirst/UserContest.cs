d7f0ba7ff61625256cc31d2824a7e1c6f1435179
Alexey Kolesnyk <monocledlama@gmail.com>
2021-04-13 21:54:09 +0300

    classes added

---
+++ EF6_CodeFirst/UserContest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6_CodeFirst
+{
+    class UserContest : DbContext
+    {
+        public UserContest() : base("DBConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
